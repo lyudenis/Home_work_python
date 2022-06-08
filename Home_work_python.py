76595626f96b3133d43e27178736a1875d0f0a62
lyudenis <lyudenis@yandex.ru>
2022-06-08 22:11:57 +0300

    дз1

---
+++ Home_work_python.py
@@ -0,0 +1,24 @@
+# 1. Элементарная арифметика
+# напишите выражение внутри print(...) в результате которого
+# на консоль должно выводиться число 12
+
+print(2+2*3+4)
+
+# 2. Объявление переменных
+# Объявите переменную name и присвойте значение "John Snow", и переменную с именем age и присвойте ей значение 29.
+
+name = "John Snow"
+age = 29
+print (name, age)
+
+# 3. Изменить код, чтобы текст выводился:
+# print("I'm a student and I'll become a "strong" programmer") #I'm a student and I'll become a "strong" programmer
+# print("C:\Users\GoodStudent") #C:\Users\GoodStudent
+# x = "My name is Agent Smith"
+# print(x[?]) #y
+# print(x[?:?:?]) #nesgt
+print ("I'm a student and I'll become a \"strong\" programmer")
+print ("C:\\Users\\GoodStudent")
+x= "My name is Agent Smith"
+print (x[1])
+print (x[3:17:3])
